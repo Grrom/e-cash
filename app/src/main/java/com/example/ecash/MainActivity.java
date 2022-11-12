34236eb0b5a06f2d9d36dc68776c044dea8cba74
Grrom <josejeromelalunio@gmail.com>
2022-11-13 06:57:48 +0800

    add login ui

--- app/src/main/java/com/example/ecash/MainActivity.java
+++ app/src/main/java/com/example/ecash/MainActivity.java
@@ -2,14 +2,22 @@ package com.example.ecash;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MaterialButton btnSignin = findViewById(R.id.btn_signin);
+
+        Intent intent = new Intent(this, );
+
+
     }
 }
